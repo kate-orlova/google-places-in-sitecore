2273d32094c55dc117e3a4dbc8f25026161b042d
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-02 23:25:58 +0100

    Initial solution structure

---
+++ src/Foundation/Importer/Importers/IImporter.cs
@@ -0,0 +1,13 @@
+﻿using Importer.Models;
+using Sitecore.Publishing;
+using System;
+using System.Collections.Generic;
+
+namespace Importer.Importers
+{
+    interface IImporter
+    {
+        IList<ImportLogEntry> Publish(Guid root, PublishMode mode = PublishMode.Smart, bool publishRelatedItems = true,
+            bool republishAll = false);
+    }
+}
---
+++ src/Foundation/Importer/Models/ImportLogEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Importer.Models
+{
+    class ImportLogEntry
+    {
+        public Guid Id { get; set; }
+
+        public string Message { get; set; }
+    }
+}
