f63a916f8f0019f8b8b7de6d02a6d4c0480419c4
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-02 23:30:43 +0100

    Added a MessageLevel property to the Importer class

--- src/Foundation/Importer/Models/ImportLogEntry.cs
+++ src/Foundation/Importer/Models/ImportLogEntry.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using Importer.Enums;
 
 namespace Importer.Models
 {
     class ImportLogEntry
     {
         public Guid Id { get; set; }
-
         public string Message { get; set; }
+        public MessageLevel Level { get; set; }
     }
 }
