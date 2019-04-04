28b8ac83b1645cc63b650ac18a6fae49356a0476
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-04 20:27:01 +0100

    Defined a SitecoreContext

--- src/Foundation/Importer/Importers/BaseImporter.cs
+++ src/Foundation/Importer/Importers/BaseImporter.cs
@@ -2,11 +2,14 @@
 using Sitecore.Publishing;
 using System;
 using System.Collections.Generic;
+using Glass.Mapper.Sc;
 
 namespace Importer.Importers
 {
     public class BaseImporter: IImporter
     {
+        protected readonly ISitecoreContext SitecoreContext;
+
         public IList<ImportLogEntry> Publish(Guid root, PublishMode mode = PublishMode.Smart, bool publishRelatedItems = true,
             bool republishAll = false)
         {
