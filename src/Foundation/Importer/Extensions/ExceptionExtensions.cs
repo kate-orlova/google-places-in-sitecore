bcaccb55caa9543114e993919ebe05dd805f348e
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-04 20:39:34 +0100

    Implemented GetAllMessages() extension for exceptions

--- src/Foundation/Importer/Extensions/ExceptionExtensions.cs
+++ src/Foundation/Importer/Extensions/ExceptionExtensions.cs
@@ -1,6 +1,24 @@
-﻿namespace Importer.Extensions
+﻿using System;
+using System.Text;
+
+namespace Importer.Extensions
 {
-    public class ExceptionExtensions
+    public static class ExceptionExtensions
     {
+        public static string GetAllMessages(this Exception exception)
+        {
+            var innerException = exception;
+
+            var sb = new StringBuilder();
+
+            while (innerException != null)
+            {
+                sb.AppendLine(innerException.Message);
+
+                innerException = innerException.InnerException;
+            }
+
+            return sb.ToString();
+        }
     }
 }
