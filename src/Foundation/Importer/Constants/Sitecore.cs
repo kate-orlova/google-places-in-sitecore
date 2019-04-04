ae24fe90bc1bd6b68b6a63b524a7a9ab72b44bfd
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-04 20:46:45 +0100

    Declared constants for Sitecore DB names

---
+++ src/Foundation/Importer/Constants/Sitecore.cs
@@ -0,0 +1,12 @@
+﻿namespace Importer.Constants
+{
+    public static class Sitecore
+    {
+        public static class Databases
+        {
+            public const string Core = "core";
+            public const string Web = "web";
+            public const string Master = "master";
+        }
+    }
+}
