fbcc89b2f93461407b8943523d233a3f09b43fb2
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-07 12:54:57 +0100

    Created an ImportJobLogger class

---
+++ src/Foundation/Importer/Loggers/ImportJobLogger.cs
@@ -0,0 +1,14 @@
+﻿using Importer.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Importer.Loggers
+{
+    public class ImportJobLogger : IImportJobLogger
+    {
+        public void LogImportResults(IList<ImportLogEntry> importResults)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
