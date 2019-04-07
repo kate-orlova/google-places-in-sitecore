613ceb6a65f4222330d764aeea3bef668758e8d1
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-07 12:59:07 +0100

    Declared a logger

--- src/Foundation/Importer/Loggers/ImportJobLogger.cs
+++ src/Foundation/Importer/Loggers/ImportJobLogger.cs
@@ -1,11 +1,18 @@
 ﻿using Importer.Models;
 using System;
 using System.Collections.Generic;
+using log4net;
 
 namespace Importer.Loggers
 {
     public class ImportJobLogger : IImportJobLogger
     {
+        private readonly ILog logger;
+        public ImportJobLogger(ILog logger)
+        {
+            this.logger = logger;
+        }
+
         public void LogImportResults(IList<ImportLogEntry> importResults)
         {
             throw new NotImplementedException();
