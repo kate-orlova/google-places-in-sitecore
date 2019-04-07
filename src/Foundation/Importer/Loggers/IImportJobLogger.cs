b65700de7ecda1a3786769e4fc9ecf37ee42db42
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-07 12:53:24 +0100

    Created an IImportJobLogger interface

---
+++ src/Foundation/Importer/Loggers/IImportJobLogger.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Importer.Models;
+
+namespace Importer.Loggers
+{
+    public interface IImportJobLogger
+    {
+        void LogImportResults(IList<ImportLogEntry> importResults);
+    }
+}
