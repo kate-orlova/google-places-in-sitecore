7129cf87500c5d71fef902b52e3247b09dee2dac
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-07 12:47:50 +0100

    Declared a log variable

--- src/Project/GooglePlacesImport/SitecronTasks/ImportGooglePlaces.cs
+++ src/Project/GooglePlacesImport/SitecronTasks/ImportGooglePlaces.cs
@@ -1,9 +1,12 @@
 ﻿using Quartz;
+using log4net;
+
 
 namespace GooglePlacesImport.SitecronTasks
 {
     public class ImportGooglePlaces : IJob
     {
+        private readonly ILog _logger;
         public void Execute(IJobExecutionContext context)
         {
             throw new System.NotImplementedException();
