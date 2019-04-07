3247d6383f0c69cd6c9d9367924fc669331d526e
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-07 12:40:44 +0100

    Inherited GooglePlacesImport from IJob Quartz's interface

--- src/Project/GooglePlacesImport/SitecronTasks/ImportGooglePlaces.cs
+++ src/Project/GooglePlacesImport/SitecronTasks/ImportGooglePlaces.cs
@@ -1,6 +1,12 @@
-﻿namespace GooglePlacesImport.SitecronTasks
+﻿using Quartz;
+
+namespace GooglePlacesImport.SitecronTasks
 {
-    public class ImportGooglePlaces
+    public class ImportGooglePlaces : IJob
     {
+        public void Execute(IJobExecutionContext context)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
