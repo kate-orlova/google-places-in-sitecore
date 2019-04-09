4108353c9f03b045d63082fe3c611700228bc35a
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-09 19:48:06 +0100

    Implemented ToGuidList() string extension

--- src/Foundation/Importer/Extensions/StringExtensions.cs
+++ src/Foundation/Importer/Extensions/StringExtensions.cs
@@ -9,13 +9,21 @@ namespace Importer.Extensions
     {
         public static string[] SafeSplit(this string value, string separator)
         {
-
             if (string.IsNullOrEmpty(value))
             {
                 return Enumerable.Empty<string>().ToArray();
             }
 
-            return value.Split(new[] { separator }, StringSplitOptions.RemoveEmptyEntries);
+            return value.Split(new[] {separator}, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static List<Guid> ToGuidList(this string value, string separator = ",")
+        {
+            return value.SafeSplit(separator)
+                .Select(x => x.HasValue() ? x.Trim() : string.Empty)
+                .Where(x => Guid.TryParse(x, out _))
+                .Select(Guid.Parse)
+                .ToList();
         }
     }
 }
