02d22074197eb1df5ca179199abaef56552a842e
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-09 19:46:49 +0100

    Implemented SafeSplit() string extension

--- src/Foundation/Importer/Extensions/StringExtensions.cs
+++ src/Foundation/Importer/Extensions/StringExtensions.cs
@@ -1,6 +1,21 @@
-﻿namespace Importer.Extensions
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Glass.Mapper;
+
+namespace Importer.Extensions
 {
-    public class StringExtensions
+    public static class StringExtensions
     {
+        public static string[] SafeSplit(this string value, string separator)
+        {
+
+            if (string.IsNullOrEmpty(value))
+            {
+                return Enumerable.Empty<string>().ToArray();
+            }
+
+            return value.Split(new[] { separator }, StringSplitOptions.RemoveEmptyEntries);
+        }
     }
 }
