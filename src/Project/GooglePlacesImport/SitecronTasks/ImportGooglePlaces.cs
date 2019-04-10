3c2417fd025db399d382999ed2a1be711f87f9e6
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-10 19:08:25 +0100

    Declared sites for publishing by rootPath

--- src/Project/GooglePlacesImport/SitecronTasks/ImportGooglePlaces.cs
+++ src/Project/GooglePlacesImport/SitecronTasks/ImportGooglePlaces.cs
@@ -1,7 +1,13 @@
-﻿using Importer.Extensions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Importer.Extensions;
 using Importer.Loggers;
 using Quartz;
 using log4net;
+using Sitecore;
+using Sitecore.Data;
+using Sitecore.Sites;
 using Sitecron.SitecronSettings;
 
 
@@ -23,6 +29,18 @@ namespace GooglePlacesImport.SitecronTasks
             var jobDataMap = context.JobDetail.JobDataMap;
             var items = jobDataMap.GetString(SitecronConstants.FieldNames.Items);
             var siteRoots = items.ToGuidList("|");
+            if (siteRoots.Any())
+            {
+                var itemsToPublish = new List<Guid>();
+                var importWasSuccessful = false;
+                foreach (var siteRoot in siteRoots)
+                {
+                    var rootPath = Database.GetDatabase(Importer.Constants.Sitecore.Databases.Master)?.GetItem(new ID(siteRoot))?.Paths.FullPath;
+                    var sites = SiteManager.GetSites()
+                        .Where(x => string.Equals(x.Properties["rootPath"], rootPath, StringComparison.OrdinalIgnoreCase))
+                        .ToList();
+                }
+            }
         }
     }
 }
