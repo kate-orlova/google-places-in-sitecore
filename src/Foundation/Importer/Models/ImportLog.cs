03431d29c14d70dbf8316e13db6945af56ce2ad5
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-10 21:17:41 +0100

    Defined ImportLog() constructor

--- src/Foundation/Importer/Models/ImportLog.cs
+++ src/Foundation/Importer/Models/ImportLog.cs
@@ -6,5 +6,10 @@ namespace Importer.Models
     {
         public List<ImportLogEntry> Entries { get; set; }
         public int ImportedItems { get; set; }
+        public ImportLog()
+        {
+            this.Entries = new List<ImportLogEntry>();
+            this.ImportedItems = 0;
+        }
     }
 }
