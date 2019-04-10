94c13d532cccee39bd54ae7253bd5f484cbfa90b
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-10 21:15:12 +0100

    Defined an Entries property in ImportLog model

--- src/Foundation/Importer/Models/ImportLog.cs
+++ src/Foundation/Importer/Models/ImportLog.cs
@@ -1,6 +1,9 @@
-﻿namespace Importer.Models
+﻿using System.Collections.Generic;
+
+namespace Importer.Models
 {
     public class ImportLog
     {
+        public List<ImportLogEntry> Entries { get; set; }
     }
 }
