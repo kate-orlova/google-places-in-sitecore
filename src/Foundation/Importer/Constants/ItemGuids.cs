2534bd93d25b5457abfb7eaf53a3e8be17b36110
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-15 22:36:34 +0100

    Defined a parent item GUID for Google Places integration

---
+++ src/Foundation/Importer/Constants/ItemGuids.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Importer.Constants
+{
+    public static class ItemGuids
+    {
+        public static Guid ItemsForGooglePlaces = new Guid("{16EF59F-1DEA-32EA-C1AB-2A5D47E80EF9}");
+    }
+}
