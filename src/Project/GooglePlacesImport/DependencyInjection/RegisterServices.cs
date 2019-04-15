2dc274c4909d2dfca10fffd79d3eff006a78ac9b
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-15 22:47:26 +0100

    Created a RegisterServices class for service configuration

---
+++ src/Project/GooglePlacesImport/DependencyInjection/RegisterServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace GooglePlacesImport.DependencyInjection
+{
+    public class RegisterServices : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
