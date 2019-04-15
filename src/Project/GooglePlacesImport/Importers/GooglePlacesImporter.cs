3a41feca0fc1ad27dd882ba6158994ab9933e5a2
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-15 22:53:54 +0100

    Created a GooglePlacesImporter class

---
+++ src/Project/GooglePlacesImport/Importers/GooglePlacesImporter.cs
@@ -0,0 +1,20 @@
+﻿using Glass.Mapper.Sc;
+using GooglePlacesImport.Interfaces;
+using Importer.Importers;
+using Importer.Models;
+using System;
+
+namespace GooglePlacesImport.Importers
+{
+    public class GooglePlacesImporter : BaseImporter, IGooglePlacesImporter
+    {
+        public GooglePlacesImporter(ISitecoreContext sitecoreContext) : base(sitecoreContext)
+        {
+        }
+
+        public ImportLog Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
