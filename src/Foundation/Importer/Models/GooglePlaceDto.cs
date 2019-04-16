94eade74b368535e0a8b8ea39357b351cb10079f
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-16 21:38:00 +0100

    Defined the basic fields for a Google Place DTO

--- src/Foundation/Importer/Models/GooglePlaceDto.cs
+++ src/Foundation/Importer/Models/GooglePlaceDto.cs
@@ -1,7 +1,13 @@
-﻿namespace Importer.Models
+﻿using System;
+
+namespace Importer.Models
 {
     public class GooglePlaceDto
     {
         public string PlaceId { get; set; }
+        public string BasicFormattedAddress { get; set; }
+        public string BasicName { get; set; }
+        public string BasicUrl { get; set; }
+        public DateTime BasicDataImported { get; set; }
     }
 }
