a0a106e94f5d5a4b63eb1de8cde1a7d58bff489e
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-17 07:52:37 +0100

    Added a BaseImportItemProcessor class

---
+++ src/Foundation/Importer/ImportProcessors/BaseImportItemProcessor.cs
@@ -0,0 +1,8 @@
+﻿using Importer.Models;
+
+namespace Importer.ImportProcessors
+{
+    public abstract class BaseImportItemProcessor<TItem, TImportObj> : IBaseImportItemProcessor<TItem, TImportObj> where TItem : GlassBase, IGlassBase
+    {
+    }
+}
