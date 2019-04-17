3025176e77d494d5c239fc2f072dc04d61da8616
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-17 07:55:04 +0100

    Specified ProcessItem()

--- src/Foundation/Importer/ImportProcessors/BaseImportItemProcessor.cs
+++ src/Foundation/Importer/ImportProcessors/BaseImportItemProcessor.cs
@@ -1,8 +1,14 @@
-﻿using Importer.Models;
+﻿using System.Collections.Generic;
+using Importer.Models;
+using Sitecore.Globalization;
 
 namespace Importer.ImportProcessors
 {
     public abstract class BaseImportItemProcessor<TItem, TImportObj> : IBaseImportItemProcessor<TItem, TImportObj> where TItem : GlassBase, IGlassBase
     {
+        public TItem ProcessItem(TImportObj importObj, IEnumerable<Language> languageVersions, string pathOverride = null)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- src/Foundation/Importer/ImportProcessors/IBaseImportItemProcessor.cs
+++ src/Foundation/Importer/ImportProcessors/IBaseImportItemProcessor.cs
@@ -1,8 +1,11 @@
-﻿using Importer.Models;
+﻿using System.Collections.Generic;
+using Importer.Models;
+using Sitecore.Globalization;
 
 namespace Importer.ImportProcessors
 {
     public interface IBaseImportItemProcessor<out TItem, TImportObj> where TItem : IGlassBase
     {
+        TItem ProcessItem(TImportObj importObj, IEnumerable<Language> languageVersions, string pathOverride = null);
     }
 }
