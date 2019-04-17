d1cd63e8d44156a5e7d3909d0700a0d0e27aaa51
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-17 07:51:39 +0100

    Created a GlassBase class

---
+++ src/Foundation/Importer/Models/GlassBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Importer.Models
+{
+    public class GlassBase
+    {
+        
+        public virtual Guid Id { get; protected set; }
+        public virtual string Name { get; set; }
+    }
+}
