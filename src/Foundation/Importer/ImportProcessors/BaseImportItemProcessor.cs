63792c337605e5958994a4bece1fba7ba23f0895
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-18 10:25:43 +0100

    Specified a context property

--- src/Foundation/Importer/ImportProcessors/BaseImportItemProcessor.cs
+++ src/Foundation/Importer/ImportProcessors/BaseImportItemProcessor.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections.Generic;
+using Glass.Mapper.Sc;
 using Importer.Models;
+using Sitecore.DependencyInjection;
 using Sitecore.Globalization;
 
 namespace Importer.ImportProcessors
 {
     public abstract class BaseImportItemProcessor<TItem, TImportObj> : IBaseImportItemProcessor<TItem, TImportObj> where TItem : GlassBase, IGlassBase
     {
+        protected ISitecoreContext Context { get; }
+
+        protected BaseImportItemProcessor(ISitecoreContext sitecoreContext, string locationPathOverride = null)
+        {
+            this.Context = sitecoreContext;
+        }
+
         public TItem ProcessItem(TImportObj importObj, IEnumerable<Language> languageVersions, string pathOverride = null)
         {
             throw new System.NotImplementedException();
