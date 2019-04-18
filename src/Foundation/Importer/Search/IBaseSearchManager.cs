8ace8a2aaf548b02000df330dbae3b34f4b27ec0
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-18 10:51:10 +0100

    Created an IBaseSearchManager interface

---
+++ src/Foundation/Importer/Search/IBaseSearchManager.cs
@@ -0,0 +1,10 @@
+﻿using Importer.Models;
+using System;
+
+namespace Importer.Search
+{
+    public interface IBaseSearchManager<out TItem> where TItem : class, IGlassBase
+    {
+        TItem FindItem(Guid itemGuid);
+    }
+}
