e634fe72317aa4f3ba43f3b1cc3db2880a190680
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-18 10:39:52 +0100

    Defined members in IGenericSitecoreItemRepository interface

--- src/Foundation/Importer/Repositories/IGenericSitecoreItemRepository.cs
+++ src/Foundation/Importer/Repositories/IGenericSitecoreItemRepository.cs
@@ -1,8 +1,24 @@
-﻿using Importer.Models;
+﻿using System;
+using System.Collections.Generic;
+using Importer.Models;
+using Sitecore.Globalization;
 
 namespace Importer.Repositories
 {
     public interface IGenericSitecoreItemRepository<TItem> where TItem : GlassBase, IGlassBase
     {
+        TItem GetByGuid(Guid id);
+
+        IEnumerable<TItem> GetByPath(string path, Language language = null, bool mapDatabaseFields = false);
+
+        TItem GetOneByQuery(string query);
+
+        IEnumerable<TItem> GetByQuery(string query, Language language = null);
+
+        TItem Create(string itemName, GlassBase parent, Language language = null);
+
+        TItem Create(string itemName, string path, Language language = null);
+
+        void Save(TItem item);
     }
 }
