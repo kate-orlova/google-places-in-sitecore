c953892de1af7f8f2b8a401f69bacc4ee6564685
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-18 10:32:39 +0100

    Specified a cache for items

--- src/Foundation/Importer/ImportProcessors/BaseImportItemProcessor.cs
+++ src/Foundation/Importer/ImportProcessors/BaseImportItemProcessor.cs
@@ -10,11 +10,14 @@ namespace Importer.ImportProcessors
     {
         protected ISitecoreContext Context { get; }
         protected readonly string LocationPathOverride;
+        protected virtual bool CacheItems => true;
+        protected IDictionary<string, IList<TItem>> ItemsCache;
 
         protected BaseImportItemProcessor(ISitecoreContext sitecoreContext, string locationPathOverride = null)
         {
             this.Context = sitecoreContext;
             this.LocationPathOverride = locationPathOverride;
+            this.ItemsCache = new Dictionary<string, IList<TItem>>();
         }
 
         public TItem ProcessItem(TImportObj importObj, IEnumerable<Language> languageVersions, string pathOverride = null)
