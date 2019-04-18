6cd37b6f8e0c6cc36c71aae0c09e12ba8851c4c0
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-18 10:42:31 +0100

    Created a GenericSitecoreItemRepository class

---
+++ src/Foundation/Importer/Repositories/GenericSitecoreItemRepository.cs
@@ -0,0 +1,45 @@
+﻿using Importer.Models;
+using Sitecore.Globalization;
+using System;
+using System.Collections.Generic;
+
+namespace Importer.Repositories
+{
+    public class GenericSitecoreItemRepository<TItem> : IGenericSitecoreItemRepository<TItem> where TItem : GlassBase, IGlassBase
+    {
+        public TItem GetByGuid(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TItem> GetByPath(string path, Language language = null, bool mapDatabaseFields = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TItem GetOneByQuery(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TItem> GetByQuery(string query, Language language = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TItem Create(string itemName, GlassBase parent, Language language = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TItem Create(string itemName, string path, Language language = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(TItem item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
