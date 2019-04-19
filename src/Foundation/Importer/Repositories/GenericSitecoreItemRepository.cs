72cba572ea0f707c86a36f9be14d3e7c48c01041
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-19 12:02:52 +0100

    Implemented GetByPath()

--- src/Foundation/Importer/Repositories/GenericSitecoreItemRepository.cs
+++ src/Foundation/Importer/Repositories/GenericSitecoreItemRepository.cs
@@ -2,13 +2,16 @@
 using Sitecore.Globalization;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Glass.Mapper.Sc;
+using Importer.Search;
 
 namespace Importer.Repositories
 {
     public class GenericSitecoreItemRepository<TItem> : IGenericSitecoreItemRepository<TItem> where TItem : GlassBase, IGlassBase
     {
         private readonly ISitecoreContext context;
+        private readonly IBaseSearchManager<TItem> searchManager;
         public TItem GetByGuid(Guid id)
         {
             return this.context.GetItem<TItem>(id, inferType: true);
@@ -16,7 +19,7 @@ namespace Importer.Repositories
 
         public IEnumerable<TItem> GetByPath(string path, Language language = null, bool mapDatabaseFields = false)
         {
-            throw new NotImplementedException();
+            return searchManager.GetAllWithTemplate(path: path, mapResults: mapDatabaseFields, language: language).ToList();
         }
 
         public TItem GetOneByQuery(string query)
