e96b67fc176f1819311a97f71859d2d21714e6f4
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-19 12:01:16 +0100

    Defined GetAllWithTemplate() in IBaseSearchManager

--- src/Foundation/Importer/Search/IBaseSearchManager.cs
+++ src/Foundation/Importer/Search/IBaseSearchManager.cs
@@ -1,10 +1,14 @@
 ﻿using Importer.Models;
 using System;
+using System.Collections.Generic;
+using Sitecore.ContentSearch;
+using Sitecore.Globalization;
 
 namespace Importer.Search
 {
     public interface IBaseSearchManager<out TItem> where TItem : class, IGlassBase
     {
         TItem FindItem(Guid itemGuid);
+        IEnumerable<TItem> GetAllWithTemplate(IProviderSearchContext context = null, bool mapResults = false, bool inferType = false, bool isLazy = false, string path = null, Language language = null);
     }
 }
