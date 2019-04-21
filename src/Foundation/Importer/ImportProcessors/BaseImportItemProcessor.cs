0bb1d8ec7cb0f997a06b954a4604a5f6445205b9
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-21 14:33:35 +0100

    Specified a generic item repository

--- src/Foundation/Importer/ImportProcessors/BaseImportItemProcessor.cs
+++ src/Foundation/Importer/ImportProcessors/BaseImportItemProcessor.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
-using Glass.Mapper.Sc;
+﻿using Glass.Mapper.Sc;
 using Importer.Models;
+using Importer.Search;
+using Microsoft.Extensions.DependencyInjection;
 using Sitecore.DependencyInjection;
 using Sitecore.Globalization;
+using System.Collections.Generic;
+using Importer.Repositories;
 
 namespace Importer.ImportProcessors
 {
@@ -12,12 +15,15 @@ namespace Importer.ImportProcessors
         protected readonly string LocationPathOverride;
         protected virtual bool CacheItems => true;
         protected IDictionary<string, IList<TItem>> ItemsCache;
+        protected IGenericSitecoreItemRepository<TItem> GenericItemRepository { get; }
 
         protected BaseImportItemProcessor(ISitecoreContext sitecoreContext, string locationPathOverride = null)
         {
             this.Context = sitecoreContext;
             this.LocationPathOverride = locationPathOverride;
             this.ItemsCache = new Dictionary<string, IList<TItem>>();
+            var searchManager = ServiceLocator.ServiceProvider.GetService<IBaseSearchManager<TItem>>();
+            this.GenericItemRepository = new GenericSitecoreItemRepository<TItem>(sitecoreContext, searchManager);
         }
 
         public TItem ProcessItem(TImportObj importObj, IEnumerable<Language> languageVersions, string pathOverride = null)
