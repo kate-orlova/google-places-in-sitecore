a687d78937be1861016ec58ce70216237f385996
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-29 22:44:26 +0100

    Created a GooglePlacesService

---
+++ src/Project/GooglePlacesImport/Services/GooglePlacesService.cs
@@ -0,0 +1,20 @@
+﻿using GooglePlacesImport.Interfaces;
+using Importer.Models;
+using System;
+using System.Collections.Generic;
+
+namespace GooglePlacesImport.Services
+{
+    public class GooglePlacesService : IGooglePlacesService
+    {
+        public IEnumerable<ItemDto> PopulateGooglePlacesIds(IEnumerable<ItemDto> existingStockists, bool reSearchPlaceId, ref List<ImportLogEntry> logEntries)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ItemDto> PopulateGooglePlacesData(IEnumerable<ItemDto> existingStockists, ref List<ImportLogEntry> logEntries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
