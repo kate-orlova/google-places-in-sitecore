07e3fe75cb8d5e55d3522524507b76826d65ecdf
Kate Orlova <kate.orlova@deleteagency.com>
2019-04-29 22:39:51 +0100

    Implemented an IGooglePlacesService interface

---
+++ src/Project/GooglePlacesImport/Interfaces/IGooglePlacesService.cs
@@ -0,0 +1,12 @@
+﻿using Importer.Models;
+using System.Collections.Generic;
+
+namespace GooglePlacesImport.Interfaces
+{
+    public interface IGooglePlacesService
+    {
+        IEnumerable<ItemDto> PopulateGooglePlacesIds(IEnumerable<ItemDto> existingStockists, bool reSearchPlaceId, ref List<ImportLogEntry> logEntries);
+        IEnumerable<ItemDto> PopulateGooglePlacesData(IEnumerable<ItemDto> existingStockists, ref List<ImportLogEntry> logEntries);
+
+    }
+}
