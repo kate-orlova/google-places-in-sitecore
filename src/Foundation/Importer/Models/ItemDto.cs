d84477f132380a9506108921e0693ec52063a6d3
Kate Orlova <kate.orlova@deleteagency.com>
2019-05-04 10:17:31 +0100

    Added coordinates properties

--- src/Foundation/Importer/Models/ItemDto.cs
+++ src/Foundation/Importer/Models/ItemDto.cs
@@ -9,5 +9,7 @@
         public string City { get; set; }
         public string County { get; set; }
         public string Postcode { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
     }
 }
