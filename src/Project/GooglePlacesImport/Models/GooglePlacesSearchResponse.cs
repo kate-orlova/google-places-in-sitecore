ecd16aa26c55a468057fdb0aad735a9f88cda6ed
Kate Orlova <kate.orlova@deleteagency.com>
2019-05-05 11:31:43 +0100

    Defined a Status property

--- src/Project/GooglePlacesImport/Models/GooglePlacesSearchResponse.cs
+++ src/Project/GooglePlacesImport/Models/GooglePlacesSearchResponse.cs
@@ -1,6 +1,10 @@
-﻿namespace GooglePlacesImport.Models
+﻿using Newtonsoft.Json;
+
+namespace GooglePlacesImport.Models
 {
     public class GooglePlacesSearchResponse
     {
+        [JsonProperty("status", NullValueHandling = NullValueHandling.Ignore)]
+        public string Status { get; set; }
     }
 }
