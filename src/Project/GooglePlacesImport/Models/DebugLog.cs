cb7a29effb84462fb5cf465057e46ea28663f3bb
Kate Orlova <kate.orlova@deleteagency.com>
2019-05-05 11:35:46 +0100

    Defined a Line property

--- src/Project/GooglePlacesImport/Models/DebugLog.cs
+++ src/Project/GooglePlacesImport/Models/DebugLog.cs
@@ -1,6 +1,11 @@
-﻿namespace GooglePlacesImport.Models
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace GooglePlacesImport.Models
 {
     public class DebugLog
     {
+        [JsonProperty("line", NullValueHandling = NullValueHandling.Ignore)]
+        public List<object> Line { get; set; }
     }
 }
