0225b36773363f45cb610e908c612f7c9fa751d7
Kate Orlova <kate.orlova@deleteagency.com>
2019-05-06 10:57:08 +0100

    Defined a PermanentlyClosed property

--- src/Project/GooglePlacesImport/Models/Candidate.cs
+++ src/Project/GooglePlacesImport/Models/Candidate.cs
@@ -6,5 +6,8 @@ namespace GooglePlacesImport.Models
     {
         [JsonProperty("place_id", NullValueHandling = NullValueHandling.Ignore)]
         public string PlaceId { get; set; }
+
+        [JsonProperty("permanently_close")]
+        public bool PermanentlyClosed { get; set; }
     }
 }
