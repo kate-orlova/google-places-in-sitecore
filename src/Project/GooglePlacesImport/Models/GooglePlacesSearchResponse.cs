c3ebc08ea03b0fcc471a0b238f7c5294b573b101
Kate Orlova <kate.orlova@deleteagency.com>
2019-05-06 10:58:52 +0100

    Defined a Candidates property

--- src/Project/GooglePlacesImport/Models/GooglePlacesSearchResponse.cs
+++ src/Project/GooglePlacesImport/Models/GooglePlacesSearchResponse.cs
@@ -1,9 +1,13 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace GooglePlacesImport.Models
 {
     public class GooglePlacesSearchResponse
     {
+        [JsonProperty("candidates", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Candidate> Candidates { get; set; }
+
         [JsonProperty("status", NullValueHandling = NullValueHandling.Ignore)]
         public string Status { get; set; }
     }
