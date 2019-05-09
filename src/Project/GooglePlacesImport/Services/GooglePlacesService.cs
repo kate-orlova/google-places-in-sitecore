e6c4f2490905b8a7e5ff0604ff3b09e0c83b9def
Kate Orlova <kate.orlova@deleteagency.com>
2019-05-09 20:22:25 +0100

    Implemented population of the contact fields

--- src/Project/GooglePlacesImport/Services/GooglePlacesService.cs
+++ src/Project/GooglePlacesImport/Services/GooglePlacesService.cs
@@ -152,12 +152,15 @@ namespace GooglePlacesImport.Services
             var key = "GOOGLE_API_KEY";
             var items = new ConcurrentBag<ItemDto>();
             var basicFields = "name,url,formatted_address";
+            var contactFields = "formatted_phone_number,opening_hours";
             Int32 basicDataCacheMinutes = 10080;
+            Int32 contactDataCacheMinutes = 10080;
 
             Parallel.ForEach(existingItems, item =>
             {
                 var fields = new List<string>();
                 if (item.GooglePlaceData.BasicDataImported.AddMinutes(basicDataCacheMinutes) < DateTime.Now) fields.Add(basicFields);
+                if (item.GooglePlaceData.ContactDataImported.AddMinutes(contactDataCacheMinutes) < DateTime.Now) fields.Add(contactFields);
             });
 
             return items;
