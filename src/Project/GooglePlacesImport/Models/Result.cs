d202c44cb278931ef7929060762fd4055ef3e5fc
Kate Orlova <kate.orlova@deleteagency.com>
2019-05-10 19:23:00 +0100

    Defined a FormattedAddress property

--- src/Project/GooglePlacesImport/Models/Result.cs
+++ src/Project/GooglePlacesImport/Models/Result.cs
@@ -1,6 +1,10 @@
-﻿namespace GooglePlacesImport.Models
+﻿using Newtonsoft.Json;
+
+namespace GooglePlacesImport.Models
 {
     public partial class Result
     {
+        [JsonProperty("formatted_address", NullValueHandling = NullValueHandling.Ignore)]
+        public string FormattedAddress { get; set; }
     }
 }
