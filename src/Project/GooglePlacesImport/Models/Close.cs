31e888a3093852015428050b6fae927983dadf42
Kate Orlova <kate.orlova@deleteagency.com>
2019-05-11 13:57:15 +0100

    Defined a Day property

--- src/Project/GooglePlacesImport/Models/Close.cs
+++ src/Project/GooglePlacesImport/Models/Close.cs
@@ -1,6 +1,10 @@
-﻿namespace GooglePlacesImport.Models
+﻿using Newtonsoft.Json;
+
+namespace GooglePlacesImport.Models
 {
     public partial class Close
     {
+        [JsonProperty("day", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Day { get; set; }
     }
 }
