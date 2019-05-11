53bf087c49618efdbd56004f0abef579929dad93
Kate Orlova <kate.orlova@deleteagency.com>
2019-05-11 13:58:44 +0100

    Defined a Close property

--- src/Project/GooglePlacesImport/Models/Period.cs
+++ src/Project/GooglePlacesImport/Models/Period.cs
@@ -1,6 +1,10 @@
-﻿namespace GooglePlacesImport.Models
+﻿using Newtonsoft.Json;
+
+namespace GooglePlacesImport.Models
 {
     public partial class Period
     {
+        [JsonProperty("close", NullValueHandling = NullValueHandling.Ignore)]
+        public Close Close { get; set; }
     }
 }
