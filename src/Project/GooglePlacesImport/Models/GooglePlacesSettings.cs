741e3cde1da96034bf4e6fc08f601724f5550cb9
Kate Orlova <kate.orlova@deleteagency.com>
2019-05-26 14:37:24 +0100

    Added a property for Google API key

--- src/Project/GooglePlacesImport/Models/GooglePlacesSettings.cs
+++ src/Project/GooglePlacesImport/Models/GooglePlacesSettings.cs
@@ -8,59 +8,67 @@ namespace GooglePlacesImport.Models
     public class GooglePlacesSettings : GlassBase
     {
         /// <summary>
+        /// Google API Key field
+        /// <para>Field Type: Single-Line Text</para>		
+        /// </summary>
+        [SitecoreField(FieldId = "{FIELD_ID}", FieldName = "Google API Key")]
+
+        public virtual string GoogleApiKey { get; set; }
+
+        /// <summary>
         /// Google Place Search Request field
         /// <para>Field Type: Single-Line Text</para>		
         /// </summary>
         [SitecoreField(FieldId = "{FIELD_ID}", FieldName = "Google Place Search Request")]
-        public string GooglePlaceSearchRequest { get; set; }
+        public virtual string GooglePlaceSearchRequest { get; set; }
 
         /// <summary>
         /// Google Place Details By Id Request field
         /// <para>Field Type: Single-Line Text</para>		
         /// </summary>
         [SitecoreField(FieldId = "{FIELD_ID}", FieldName = "Google Place Details By Id Request")]
-        public string GooglePlaceDetailsByIdRequest { get; set; }
+        public virtual string GooglePlaceDetailsByIdRequest { get; set; }
 
         /// <summary>
         /// Google Place Basic Data Fields field
         /// <para>Field Type: Single-Line Text</para>		
         /// </summary>
         [SitecoreField(FieldId = "{FIELD_ID}", FieldName = "Google Place Basic Data Fields")]
-        public string GooglePlaceBasicDataFields { get; set; }
+        public virtual string GooglePlaceBasicDataFields { get; set; }
 
         /// <summary>
         /// Google Place Basic Data Cache Minutes field
         /// <para>Field Type: Integer</para>		
         /// </summary>
         [SitecoreField(FieldId = "{FIELD_ID}", FieldName = "Google Place Basic Data Cache Minutes")]
-        public Int32 GooglePlaceBasicDataCacheMinutes { get; set; }
+        public virtual Int32 GooglePlaceBasicDataCacheMinutes { get; set; }
 
         /// <summary>
         /// Google Place Contact Data Fields field
         /// <para>Field Type: Single-Line Text</para>		
         /// </summary>
         [SitecoreField(FieldId = "{FIELD_ID}", FieldName = "Google Place Contact Data Fields")]
-        public string GooglePlaceContactDataFields { get; set; }
+        public virtual string GooglePlaceContactDataFields { get; set; }
 
         /// <summary>
         /// Google Place Contact Data Cache Minutes field
         /// <para>Field Type: Integer</para>		
         /// </summary>
         [SitecoreField(FieldId = "{FIELD_ID}", FieldName = "Google Place Contact Data Cache Minutes")]
-        public Int32 GooglePlaceContactDataCacheMinutes { get; set; }
+        public virtual Int32 GooglePlaceContactDataCacheMinutes { get; set; }
 
         /// <summary>
         /// Google Place Atmosphere Data Fields field
         /// <para>Field Type: Single-Line Text</para>		
         /// </summary>
         [SitecoreField(FieldId = "{FIELD_ID}", FieldName = "Google Place Atmosphere Data Fields")]
-        public string GooglePlaceAtmosphereDataFields { get; set; }
+        public virtual string GooglePlaceAtmosphereDataFields { get; set; }
 
         /// <summary>
         /// Google Place Atmosphere Data Cache Minutes field
         /// <para>Field Type: Integer</para>		
         /// </summary>
         [SitecoreField(FieldId = "{FIELD_ID}", FieldName = "Google Place Atmosphere Data Cache Minutes")]
-        public Int32 GooglePlaceAtmosphereDataCacheMinutes { get; set; }
+        public virtual Int32 GooglePlaceAtmosphereDataCacheMinutes { get; set; }
     }
 }
